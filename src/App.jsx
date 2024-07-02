2520b92580a216ebd1800aa2e149f71552d8533d
Akash Kumar <akashkumar.dev00@gmail.com>
2024-07-02 11:28:41 +0530

    feat: Navbar created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div class="main-content">
+        <Navbar />
+        <main>
+
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
